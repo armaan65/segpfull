e71f935bd67243417d17391435696f1046a3ec92
Armaan Farhan <armaanfarhan2004@gmail.com>
2024-02-27 20:05:42 +0800

    Improved UI of Scorten calculator page

--- lib/screens/scorten.dart
+++ lib/screens/scorten.dart
@@ -7,6 +7,8 @@ class ScortenCalculatorPage extends StatefulWidget {
 }
 
 class _ScortenCalculatorPageState extends State<ScortenCalculatorPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   TextEditingController ageController = TextEditingController();
   TextEditingController malignancyController = TextEditingController();
   TextEditingController heartRateController = TextEditingController();
@@ -15,159 +17,241 @@ class _ScortenCalculatorPageState extends State<ScortenCalculatorPage> {
   TextEditingController glucoseController = TextEditingController();
   TextEditingController bicarbonateController = TextEditingController();
 
-  double scortenResult = 0;
+  int scortenResult = 0;
   double riskFactorResult = 0;
 
   @override
   Widget build(BuildContext context) {
+    final TextStyle customFont = TextStyle(
+      fontFamily: 'Roboto',
+      fontSize: 16,
+    );
+
+    final ColorScheme colorScheme = ColorScheme(
+      primary: Color(0xFFA7E6FF),
+      secondary: Color(0xFF53CADA),
+      surface: Colors.white,
+      background: Colors.white,
+      error: Colors.red,
+      onPrimary: Colors.black,
+      onSecondary: Colors.black,
+      onSurface: Colors.black,
+      onBackground: Colors.black,
+      onError: Colors.white,
+      brightness: Brightness.light,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('SCORTEN Calculator'),
+        backgroundColor: colorScheme.primary,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Enter Patient Information:'),
-            TextField(
-              controller: ageController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Age'),
-            ),
-            TextField(
-              controller: malignancyController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Malignancy (0 or 1)'),
-            ),
-            TextField(
-              controller: heartRateController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Heart Rate(beats/minute)'),
-            ),
-            TextField(
-              controller: bsaController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Body Surface Area (%)'),
-            ),
-            TextField(
-              controller: ureaController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Serum Urea(mmol/L)'),
-            ),
-            TextField(
-              controller: glucoseController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Serum Glucose(mmol/L)'),
-            ),
-            TextField(
-              controller: bicarbonateController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Serum Bicarbonate(mmol/L)'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                calculateResults();
-              },
-              child: Text('Calculate'),
-            ),
-            SizedBox(height: 20),
-            Text('SCORTEN Score: $scortenResult'),
-            Text('Calculated Risk Factor: $riskFactorResult%'),
-            SizedBox(height: 20),
-            // Remove the Navigator widget that wraps the ElevatedButton widget
-            // Use the Navigator widget that is provided by the Scaffold widget
-            ElevatedButton(
-              // Use the onPressed property to check if all the details are filled
-              onPressed: () {
-                // Get the values of the text fields
-                String age = ageController.text;
-                String malignancy = malignancyController.text;
-                String heartRate = heartRateController.text;
-                String bsa = bsaController.text;
-                String urea = ureaController.text;
-                String glucose = glucoseController.text;
-                String bicarbonate = bicarbonateController.text;
-
-                // Check if any of the text fields is empty
-                if (age.isEmpty ||
-                    malignancy.isEmpty ||
-                    heartRate.isEmpty ||
-                    bsa.isEmpty ||
-                    urea.isEmpty ||
-                    glucose.isEmpty ||
-                    bicarbonate.isEmpty) {
-                  // If yes, show a SnackBar widget with a message to the user
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Please fill all the details'),
-                    ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Enter Patient Information:',
+                style: customFont.copyWith(fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                controller: ageController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Age',
+                  labelStyle: customFont,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter age';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: malignancyController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Malignancy (0 or 1)',
+                  labelStyle: customFont,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter malignancy';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: heartRateController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Heart Rate(beats/minute)',
+                  labelStyle: customFont,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter heart rate';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: bsaController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Body Surface Area (%)',
+                  labelStyle: customFont,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter body surface area';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: ureaController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Serum Urea(mmol/L)',
+                  labelStyle: customFont,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter serum urea';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: glucoseController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Serum Glucose(mmol/L)',
+                  labelStyle: customFont,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter serum glucose';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: bicarbonateController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Serum Bicarbonate(mmol/L)',
+                  labelStyle: customFont,
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter serum bicarbonate';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  _formKey.currentState?.validate();
+                  if (_formKey.currentState?.validate() ?? false) {
+                    calculateResults();
+                  }
+                },
+                child: Text('Calculate'),
+                style: ElevatedButton.styleFrom(
+                  primary: colorScheme.secondary,
+                  onPrimary: colorScheme.onSecondary,
+                ),
+              ),
+              SizedBox(height: 20),
+              StatefulBuilder(
+                builder: (BuildContext context, StateSetter setState) {
+                  return Column(
+                    children: [
+                      Text(
+                        'SCORTEN Score: $scortenResult',
+                        style: customFont.copyWith(fontSize: 18),
+                      ),
+                      Text(
+                        'Calculated Risk Factor: $riskFactorResult%',
+                        style: customFont.copyWith(fontSize: 18),
+                      ),
+                    ],
                   );
-                } else {
-                  // If no, navigate to the BasePage widget
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return BasePage();
-                      },
-                    ),
-                  );
-                }
-              },
-              child: Text('Next'),
-            ),
-          ],
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState?.validate() ?? false) {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return BasePage();
+                        },
+                      ),
+                    );
+                  }
+                },
+                child: Text('Next'),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: colorScheme.secondary,
+                  backgroundColor: colorScheme.onSecondary,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
   void calculateResults() {
-    double age = double.tryParse(ageController.text) ?? 0;
-    double malignancy = double.tryParse(malignancyController.text) ?? 0;
-    double heartRate = double.tryParse(heartRateController.text) ?? 0;
-    double bsa = double.tryParse(bsaController.text) ?? 0;
-    double urea = double.tryParse(ureaController.text) ?? 0;
-    double glucose = double.tryParse(glucoseController.text) ?? 0;
-    double bicarbonate = double.tryParse(bicarbonateController.text) ?? 0;
+    int age = int.parse(ageController.text);
+    int malignancy = int.parse(malignancyController.text);
+    int heartRate = int.parse(heartRateController.text);
+    int bsa = int.parse(bsaController.text);
+    int urea = int.parse(ureaController.text);
+    int glucose = int.parse(glucoseController.text);
+    int bicarbonate = int.parse(bicarbonateController.text);
 
     setState(() {
       scortenResult = calculateScorten(age, malignancy, heartRate, bsa, urea, glucose, bicarbonate);
-      riskFactorResult = calculateriskfactor(age, malignancy, heartRate, bsa, urea, glucose, bicarbonate);
+      riskFactorResult = calculateRiskFactor(scortenResult);
     });
   }
 
-  double calculateScorten(double age, double malignancy, double heartRate,
-    double bsa, double urea, double glucose, double bicarbonate) {
-  double agePoints = (age >= 40) ? 1 : 0;
-  double heartRatePoints = (heartRate >= 120) ? 1 : 0;
-  double bsaPoints = (bsa >= 10) ? 1 : 0;
-  double ureaPoints = (urea > 10) ? 1 : 0;
-  double glucosePoints = (glucose > 14) ? 1 : 0;
-  double bicarbonatePoints = (bicarbonate < 20) ? 1 : 0;
-  double malignancyPoints = (malignancy == 1) ? 1 : 0;
+  int calculateScorten(int age, int malignancy, int heartRate, int bsa, int urea, int glucose, int bicarbonate) {
+    int agePoints = (age >= 40) ? 1 : 0;
+    int heartRatePoints = (heartRate >= 120) ? 1 : 0;
+    int bsaPoints = (bsa >= 10) ? 1 : 0;
+    int ureaPoints = (urea > 10) ? 1 : 0;
+    int glucosePoints = (glucose > 14) ? 1 : 0;
+    int bicarbonatePoints = (bicarbonate < 20) ? 1 : 0;
+    int malignancyPoints = (malignancy == 1) ? 1 : 0;
 
-  return agePoints + ureaPoints + glucosePoints + bsaPoints + bicarbonatePoints + heartRatePoints + malignancyPoints;
-}
-
-double calculateriskfactor(double age, double malignancy, double heartRate,
-    double bsa, double urea, double glucose, double bicarbonate) {
-  double scortenScore = calculateScorten(age, malignancy, heartRate, bsa, urea, glucose, bicarbonate);
-  double riskfactor;
-
-  if (scortenScore <= 1) {
-    riskfactor = 3.2;
-  } else if (scortenScore == 2) {
-    riskfactor = 12.1;
-  } else if (scortenScore == 3) {
-    riskfactor = 35.3;
-  } else if (scortenScore == 4) {
-    riskfactor = 58.3;
-  } else {
-    riskfactor = 90;
+    return agePoints + ureaPoints + glucosePoints + bsaPoints + bicarbonatePoints + heartRatePoints + malignancyPoints;
   }
 
-  return riskfactor;
-}
+  double calculateRiskFactor(int scortenScore) {
+    switch (scortenScore) {
+      case 0:
+      case 1:
+        return 3.2;
+      case 2:
+        return 12.1;
+      case 3:
+        return 35.3;
+      case 4:
+        return 58.3;
+      default:
+        return 90;
+    }
+  }
 }
