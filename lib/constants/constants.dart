3ba6fee67b2d8286ae55ae87d20cd17d1b43421c
Armaan Farhan <armaanfarhan2004@gmail.com>
2024-03-20 14:42:40 +0800

    Made changes to the chat

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,3 +1,17 @@
+import 'package:appwrite/appwrite.dart';
+
+
+
 const String APPWRITE_PROJECT_ID = "segp";
 const String APPWRITE_DATABASE_ID = "65b204306d142776c878";
 const String APPWRITE_URL = "https://cloud.appwrite.io/v1";
+const String COLLECTION_ID_MESSAGES = '65d2dc09e10cb3467746';
+const String API_ENDPOINT = 'YOUR_API_ENDPOINT';
+
+final Client client = Client()
+  .setEndpoint(APPWRITE_URL)
+  .setProject(APPWRITE_PROJECT_ID);
+
+
+
+final Databases databases = Databases(client);
